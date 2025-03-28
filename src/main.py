12966b2875daefcd6f5bd0e00fe2d34eae1e1f24
bobur-IAdeveloper <dxrk.trade@gmail.com>
2025-03-28 22:46:46 +0500

    first project

--- src/main.py
+++ src/main.py
@@ -1,29 +1,29 @@
 from analyzer import most_sold_products
-from visualizer import show_top_products  # plt emas, funksiya import qilamiz
+from visualizer import show_top_products  # Import function, not plt
 
 def main():
     while True:
-        print("\n1 - Eng ko'p sotilgan mahsulotlar")
-        print("2 - Sotuv statistikasini ko'rish")
-        print("3 - Chiqish")
+        print("\n1 - Most sold products")
+        print("2 - View sales statistics")
+        print("3 - Exit")
 
         choice = input(">>> ")
 
         if choice == '1':
             top_products = most_sold_products()
             if not top_products:
-                print("Ma'lumotlar yuq!")
+                print("No data available!")
             else:    
-                print("\n Eng ko‘p sotilgan mahsulotlar:")
+                print("\nMost sold products:")
                 for name, count in top_products.items():
-                  print(f"  {name}: {count} ta sotilgan")
+                    print(f"  {name}: {count} sold")
         elif choice == '2':
             show_top_products()
-            print("Diagramma saqlandi!")
+            print("Chart saved!")
         elif choice == '3':
             break
         else:
-            print("Noto'g'ri tanlov! Qayta urinib ko'ring!")
+            print("Invalid choice! Please try again!")
 
 if __name__ == "__main__":
     main()
--- src/visualizer.py
+++ src/visualizer.py
@@ -4,15 +4,16 @@ from analyzer import most_sold_products
 top_products = most_sold_products()
 
 if not top_products:
-    print("Eng ko‘p sotilgan mahsulotlar haqida ma’lumot yo‘q.")
+    print("No data available for most sold products.")
 else:
     labels, values = zip(*top_products.items())
 
     def show_top_products():
         plt.figure(figsize=(10, 5))
         plt.bar(labels, values, color='skyblue')
-        plt.xlabel("Mahsulotlar")
-        plt.ylabel("Sotilgan soni")
-        plt.title("Eng ko‘p sotilgan mahsulotlar")
+        plt.xlabel("Products")
+        plt.ylabel("Units Sold")
+        plt.title("Most Sold Products")
         plt.xticks(rotation=45, ha="right")
         plt.savefig("top_products.png", dpi=300, bbox_inches="tight")
+
